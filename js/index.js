99c9e18124cb1c2b0941f470b1a8158baaf0c88d
Hina Awan <65899846+HinaAwan01@users.noreply.github.com>
2020-07-11 16:46:00 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,36 +1,24 @@
-// https://regexlib.com/?AspxAutoDetectCookieSupport=1
-
 
 let fn=document.getElementById('fullname');
 let em = document.getElementById('email');
 let msg=document.getElementById('message');
 let pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-
-
-// define the event-target-object
-let formSubmission = document.getElementById('submit-data');
+let formSubmission = document.getElementById('submit-data');  // define the event-target-object
 
 // event-handler
-function handleForm() {
-	
+function handleForm() {	
 	// empty buckets, one for inputs, the other for errors
 	let uinput = {};
 	let errors = [];
 	
-	// validating if user input exist at all
-    
+	// validating if user input exist at all    
     if (fn.value !== '') {				
-				
-            uinput.fullname = fn.value;		
-		}
-		
-	else {
+            uinput.fullname = fn.value;	
+		} else {
 		errors.push('Fullname is empty!');
-	}
+	}    
     
-    
-	if (em.value !== '') {
-		
+	if (em.value !== '') {		
 		// validate email format
 		if (pattern.test(em.value)) {
             uinput.email = em.value;
@@ -40,26 +28,20 @@ function handleForm() {
 		
 	} else {
 		errors.push('Email is empty!');
-	}
-    
-    
+	}    
     
     if (msg.value !== '') {			
             uinput.message = msg.value;		
-		}
-		
-	else {
+		} else {
 		errors.push('Message is empty!');
-	}
-	
+	}	
 	
 	// printing either feedback or errors
 	if (errors.length === 0) {
 		console.log('COLLECTED DATA', uinput);
 	} else {
 		console.log('ERRORS', errors);
-	}
-	
+	}	
 }
 
 formSubmission.addEventListener('click', handleForm);
